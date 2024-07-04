ab4b0db92f02bf207d907bc1b99843eeabe0f626
joe-principe <j.principe343@gmail.com>
2024-07-04 13:21:33 -0700

    Completed chapter 4

--- ch04/src/hello_window.c
+++ ch04/src/hello_window.c
@@ -1,12 +1,79 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <time.h>
+
+/**
+ * @brief The function called whenever the viewport is resized
+ *
+ * @param[in, out] window The GLFW window that is being resized
+ * @param[in] width The new width of the window in pixels
+ * @param[in] height The new height of the window in pixels
+ */
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+/**
+ * @brief Responds to any input from the user on the current window
+ *
+ * @param[in] window The GLFW window whose input is being read
+ */
+void process_input(GLFWwindow *window);
 
 int
 main(void)
 {
-    printf("Hello, world!\n");
+    GLFWwindow *window = NULL;
+
+    /* Instantiate the GLFW window */
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(800, 600, "Hello window", NULL, NULL);
+
+    if (window == NULL) {
+        fprintf(stderr, "Error: Failed to create GLFW window\n");
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Error: Failed to initialize GLAD\n");
+        exit(EXIT_FAILURE);
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
+
+void 
+framebuffer_size_callback(GLFWwindow *window, int width, int height) 
+{
+    glViewport(0, 0, width, height);
+}
+
+void
+process_input(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+/* EOF */
