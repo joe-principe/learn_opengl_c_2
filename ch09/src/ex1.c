24f6c22e7bc0e4ea17e662cbad9c8399591ba9f6
joe-principe <j.principe343@gmail.com>
2024-07-12 21:41:10 -0700

    Finished chapter 9 exercises.

---
+++ ch09/src/ex1.c
@@ -0,0 +1,286 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../include/shader.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../include/stb_image.h"
+
+#include <cglm/cglm.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+/**
+ * @brief The function called whenever the viewport is resized
+ *
+ * @param[in, out] window The GLFW window that is being resized
+ * @param[in] width The new width of the window in pixels
+ * @param[in] height The new height of the window in pixels
+ */
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+/**
+ * @brief Responds to any input from the user on the current window
+ *
+ * @param[in] window The GLFW window whose input is being read
+ */
+void process_input(GLFWwindow *window);
+
+int
+main(void)
+{
+    unsigned int VBO;
+    unsigned int VAO;
+
+    shader sh;
+
+    const char *vert_shader_path = "shaders/shader.vs";
+    const char *frag_shader_path = "shaders/shader.fs";
+
+    GLFWwindow *window = NULL;
+
+    int width;
+    int height;
+    int num_channels;
+
+    unsigned char *wall_texture_data = NULL;
+    unsigned char *face_texture_data = NULL;
+
+    const char *wall_texture_path = "res/container.jpg";
+    const char *face_texture_path = "res/awesomeface.png";
+
+    unsigned int texture1;
+    unsigned int texture2;
+
+    CGLM_ALIGN_MAT mat4 model = GLM_MAT4_IDENTITY_INIT;
+    CGLM_ALIGN_MAT mat4 view = GLM_MAT4_IDENTITY_INIT;
+    CGLM_ALIGN_MAT mat4 projection = GLM_MAT4_IDENTITY_INIT;
+
+    unsigned int i;
+    float angle;
+
+    float fov = 90.0f;
+    float aspect_ratio = 16.0f / 9.0f;
+
+    float vertices[] = {
+        /* x      y      z     u     v */
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f
+    };
+
+    float cube_positions[][3] = {
+        { 0.0f,  0.0f,   0.0f},
+        { 2.0f,  5.0f, -15.0f},
+        {-1.5f, -2.2f,  -2.5f},
+        {-3.8f, -2.0f, -12.3f},
+        { 2.4f, -0.4f,  -3.5f},
+        {-1.7f,  3.0f,  -7.5f},
+        { 1.3f, -3.0f,  -2.5f},
+        { 1.5f,  2.0f,  -2.5f},
+        { 1.5f,  0.2f,  -1.5f},
+        {-1.3f,  1.0f,  -1.5f}
+    };
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(800, 600, "Textures", NULL, NULL);
+
+    if (window == NULL) {
+        fprintf(stderr, "Error: Failed to create GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Error: Failed to initialize GLAD\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glViewport(0, 0, 800, 600);
+    glEnable(GL_DEPTH_TEST);
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                          (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_set_flip_vertically_on_load(true);
+
+    wall_texture_data = stbi_load(wall_texture_path, &width, &height,
+                                  &num_channels, 0);
+
+    if (wall_texture_data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                     GL_UNSIGNED_BYTE, wall_texture_data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        fprintf(stderr, "Error: Failed to load texture: %s", wall_texture_path);
+    }
+
+    stbi_image_free(wall_texture_data);
+    wall_texture_data = NULL;
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    face_texture_data = stbi_load(face_texture_path, &width, &height,
+                                  &num_channels, 0);
+
+    if (face_texture_data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA,
+                     GL_UNSIGNED_BYTE, face_texture_data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        fprintf(stderr, "Error: Failed to load texture: %s", face_texture_path);
+    }
+
+    stbi_image_free(face_texture_data);
+    face_texture_data = NULL;
+
+    create_shader(&sh, vert_shader_path, frag_shader_path);
+    glUseProgram(sh.ID);
+    glUniform1i(glGetUniformLocation(sh.ID, "texture1"), 0);
+    glUniform1i(glGetUniformLocation(sh.ID, "texture2"), 1);
+
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(sh.ID);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        glBindVertexArray(VAO);
+
+        for (i = 0; i < 10; i++) {
+            glm_mat4_identity(model);
+            glm_translate(model, cube_positions[i]);
+
+            angle = 20.0f * i;
+            glm_rotate(model, glm_rad(angle), (vec3){1.0f, 0.3f, 0.5f});
+
+            glUniformMatrix4fv(glGetUniformLocation(sh.ID, "model"), 1,
+                               GL_FALSE, (float *)model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        glm_mat4_identity(view);
+        glm_translate(view, (vec3){0.0f, 0.0f, -3.0f});
+
+        glm_mat4_identity(projection);
+        glm_perspective(glm_rad(fov), aspect_ratio, 0.1f, 100.0f, projection);
+
+        glUniformMatrix4fv(glGetUniformLocation(sh.ID, "view"), 1, GL_FALSE,
+                           (float *)view);
+        glUniformMatrix4fv(glGetUniformLocation(sh.ID, "projection"), 1,
+                           GL_FALSE, (float *)projection);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(sh.ID);
+
+    glfwTerminate();
+    return 0;
+}
+
+void 
+framebuffer_size_callback(GLFWwindow *window, int width, int height) 
+{
+    glViewport(0, 0, width, height);
+}
+
+void
+process_input(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+/* EOF */
